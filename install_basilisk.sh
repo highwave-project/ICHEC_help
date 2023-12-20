35f6d5d7778bac9522e5337ca32c8fae60cd8c44
Ryan Smith <r97smith@gmail.com>
2023-12-20 14:17:45 +0000

    suppress all dep outputs but the errors

--- install_basilisk.sh
+++ install_basilisk.sh
@@ -9,24 +9,24 @@ fi
 if [[ ! -z $LOCAL_INSTALL ]]; then
     echo "Local installation with access to sudo for installing"
     if which apt; then
-        sudo apt update
-        sudo apt install darcs make gawk gfortran gnuplot imagemagick ffmpeg graphviz valgrind gifsicle pstoedit mesa-utils libgl-dev freeglut3 freeglut3-dev
+        sudo apt update >/dev/null
+        sudo apt install darcs make gawk gfortran gnuplot imagemagick ffmpeg graphviz valgrind gifsicle pstoedit mesa-utils libgl-dev freeglut3 freeglut3-dev >/dev/null
     elif which pacman; then
         sudo pacman -S darcs make gawk gcc-fortran gnuplot imagemagick ffmpeg graphviz valgrind gifsicle pstoedit
     fi
     # Clone the Basilisk repo
-    darcs clone --lazy http://basilisk.fr/basilisk
+    darcs clone --lazy http://basilisk.fr/basilisk >/dev/null
 else
     echo "Installation with no access to sudo"
     wget http://basilisk.fr/basilisk/basilisk.tar.gz
-    tar xzf basilisk.tar.gz
+    tar xzf basilisk.tar.gz >/dev/null
 fi
 
 echo "Building Basilisk..."
 cd basilisk/src
 ln -s config.gcc config
 make -k -j $(nproc) >/dev/null
-make   # incase of any failures from previous command
+make >/dev/null  # incase of any failures from previous command
 
 if [[ -e "$HOME/.zshrc" ]]; then
     shellrc="$HOME/.zshrc"
@@ -50,7 +50,7 @@ if [[ ! -z $BUILD_GRAPHICS ]]; then
         if ! which nasm; then # Install asm compiler
             echo "---------------- Asm"
             cd $HOME/ffmpeg_sources
-            wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2 
+            wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2 >/dev/null
             tar xjvf nasm-2.15.05.tar.bz2 >/dev/null
             cd nasm-2.15.05 
             ./autogen.sh >/dev/null
@@ -62,7 +62,7 @@ if [[ ! -z $BUILD_GRAPHICS ]]; then
         if ! which x264; then # Install support for x264 video encoding
             echo "---------------- x264"
             cd $HOME/ffmpeg_sources && \
-            git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
+            git clone --depth 1 https://code.videolan.org/videolan/x264.git >/dev/null
             cd x264 && \
             PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic >/dev/null
             PATH="$HOME/bin:$PATH" make -j $(nproc) >/dev/null
@@ -72,7 +72,7 @@ if [[ ! -z $BUILD_GRAPHICS ]]; then
         if ! which x265; then # Install support for x265 video encoding
             echo "---------------- x265"
             cd $HOME/ffmpeg_sources && \
-            wget -O x265.tar.bz2 https://bitbucket.org/multicoreware/x265_git/get/master.tar.bz2 && \
+            wget -O x265.tar.bz2 https://bitbucket.org/multicoreware/x265_git/get/master.tar.bz2 >/dev/null
             tar xjvf x265.tar.bz2 >/dev/null
             cd multicoreware*/build/linux && \
             PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source >/dev/null
@@ -82,7 +82,7 @@ if [[ ! -z $BUILD_GRAPHICS ]]; then
 
         echo "---------------- FFMPEG"
         cd $HOME/ffmpeg_sources && \
-        wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
+        wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 >/dev/null
         tar xjvf ffmpeg-snapshot.tar.bz2 >/dev/null
         cd ffmpeg && \
         PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
@@ -102,7 +102,7 @@ if [[ ! -z $BUILD_GRAPHICS ]]; then
 
     cd $HOME
     echo "---------------- OSMESA"
-    wget http://basilisk.fr/src/gl/mesa-17.2.4.tar.gz
+    wget http://basilisk.fr/src/gl/mesa-17.2.4.tar.gz >/dev/null
     tar xzvf mesa-17.2.4.tar.gz >/dev/null
     cd mesa-17.2.4
     ./configure --prefix=$HOME/local --enable-osmesa \
@@ -113,7 +113,7 @@ if [[ ! -z $BUILD_GRAPHICS ]]; then
 
     cd $HOME
     echo "---------------- GLU"
-    wget http://basilisk.fr/src/gl/glu-9.0.0.tar.gz
+    wget http://basilisk.fr/src/gl/glu-9.0.0.tar.gz >/dev/null
     tar xzvf glu-9.0.0.tar.gz >/dev/null
     cd glu-9.0.0
     ./configure --prefix=$HOME/local >/dev/null
